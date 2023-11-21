3392e62ce0a5bf5b53a2a348e7b476dfc3986185
Reji Prabaharan Stephny Seshani <87108657+Stephny17@users.noreply.github.com>
2023-11-21 21:26:39 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+
+import "bootstrap/dist/css/bootstrap.min.css"
+import Signup from './component/Signup'
+import "./App.css"
+
+
+function App() {
+  return <div className="App">
+    <Signup></Signup>
+  </div>
+}
+
+export default App
